b3f93c1ae8000fe7ce3d6532f2bac07bfaad745b
maheshbasnet089 <basnetmanish089@gmail.com>
2023-08-15 17:02:15 +0545

    rever config

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,23 +1,8 @@
-// module.exports = {
-//   HOST: "localhost",
-//   USER: "root",
-//   PASSWORD: "",
-//   DB: "cms", // DATABASE NAME 
-//   dialect: "mysql", // Kun variation of sql use garney
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// };
-
 module.exports = {
-  HOST: "containers-us-west-62.railway.app",
+  HOST: "localhost",
   USER: "root",
-  PORT : 7782,
-  PASSWORD: "aWtkSpwQJY20FGIECYPo",
-  DB: "railway", // DATABASE NAME 
+  PASSWORD: "",
+  DB: "cms", // DATABASE NAME
   dialect: "mysql", // Kun variation of sql use garney
   pool: {
     max: 5,
